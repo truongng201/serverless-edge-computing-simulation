f3c7a11efefc75f619203d8d1b790b803e5ef32d
cei-server <truongng201@gmail.com>
2025-11-25 10:01:51 +0700

    minor

--- serverless-sim/function_template/handler.py
+++ serverless-sim/function_template/handler.py
@@ -1,2 +1,6 @@
+import time
+
 def handler(event, context):
+    print("Simulate an simple execution with sleeping")
+    time.sleep(5)
     return f"Say hi to this simulation. Event: {event}, Context: {context}"
