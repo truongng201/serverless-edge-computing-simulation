fcb329dfb5c0eb6a3f75c9aa368963c468046ea9
cei-server <truongng201@gmail.com>
2026-05-02 10:40:33 +0700

    new exp 02/05/2026

--- serverless-sim/run_experiments.py
+++ serverless-sim/run_experiments.py
@@ -523,7 +523,7 @@ class ExperimentRunner:
     
     def run_comprehensive_experiments(self, user_ranges = [], edge_ranges = [], algorithms = [], experiment_duration = 100):
         if not user_ranges:
-            user_ranges = [500]  # 100 users
+            user_ranges = [1000]  # 100 users
         if not edge_ranges:
             edge_ranges = [200]
         if not algorithms:
